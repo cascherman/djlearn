44d1d0d4c60b5d267988d4272d7dc3cac247c6c6
Chris <c.ascherman@gmail.com>
2025-02-22 22:31:30 -0500

    added navbar

--- main/views.py
+++ main/views.py
@@ -1,8 +1,5 @@
 from django.shortcuts import render, HttpResponse
 
-def main(request):
-    return render(request, "main/base.html")
-
 def body(request):
     list_of_items = [
         "blog", "to do list", "budget maker", "calendar", "navbar", "POST function"
