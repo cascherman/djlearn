4dafd20f52cf7d4e1476a7b97bdf0e4c9b2d6272
Chris <c.ascherman@gmail.com>
2025-11-02 16:49:47 -0500

    nov 2 2025

--- djlearn/settings.py
+++ djlearn/settings.py
@@ -17,7 +17,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist
+
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-3mpagq&&7m_)&**p04jkyvyyfeuk7a@j=ive4+yj1#w3ov41^5'
